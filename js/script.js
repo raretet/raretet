7a4ced81c941f582b24b50a4294e0309612a321b
Danya <rassadnev200777@gmail.com>
2022-03-05 23:45:55 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+//alert('hello');
+
+//const result = confirm('Are you here?');
+//console.log (result);
+
+//const answer = prompt('Are you 18?', '');
+//console.log (answer + 5);
+
+// const answers = [];
+
+//  answers[0] = prompt('how are you?', '');
+//  answers[1] = prompt('how are b?', '');
+//  answers[2] = prompt('how are a?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const categoty = 'toys';
+
+// console.log(`https://someurl.com/${categoty}`);
+
+// const user = 'ivan';
+
+// alert (`hello${user}`);
+
+// incr++;
+
+// decr--;
+
+
+// const isChecked = 1,
+//       isClosed = true;
+
+// console.log (isChecked || isClosed);
+
+
