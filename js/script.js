e0ec709c14a792b07662e5e87cc45b14e423c0f5
Danya <rassadnev200777@gmail.com>
2022-03-12 18:22:38 +0300

    lesson_17

--- js/script.js
+++ js/script.js
@@ -1,156 +1,39 @@
 "use strict";
 
-// alert('hello');
-
-// const result = confirm('Are you here?');
-//console.log (result);
-
-//const answer = prompt('Are you 18?', '');
-//console.log (answer + 5);
-
-// const answers = [];
-
-//  answers[0] = prompt('how are you?', '');
-//  answers[1] = prompt('how are b?', '');
-//  answers[2] = prompt('how are a?', '');
-
-// console.log(typeof(answers));
-// console.log(typeof(null));
-
-// const categoty = 'toys';
-
-// console.log(`https://someurl.com/${categoty}`);
-
-// const user = 'ivan';
-
-// alert (`hello${user}`);
-
-// incr++;
-
-// decr--;
-
-
-// const isChecked = 1,
-//       isClosed = true;
-
-// console.log (isChecked || isClosed);
-
-
-// if (9 === 4 * 5 - 6) {
-// console.log (1);
-// } else {
-//     console.log(11);
-// }
-
-// if (mum < 49) {
-//     console.log ('error');
-// } else if (mum > 100) {
-//     console.log ('lot');
-// } else {
-//     console.log ('ok');
-// }
-
-// (+'4' === 4) ? console.log ('ok') : console.log ('lot');
-
-//  const num = 50;
-
-//  switch (num) {
-//     case 49: 
-//         console.log ('ok');
-//         break;
-//     case 100: 
-//         console.log ('ok');
-//         break;
-//     case 30: 
-//         console.log ('ppp');
-//         break;
-//     default : 
-//         console.log ('no');
-//         break;
-//  }
- 
-// while (num <= 55) {
-//     console.log('num');
-//     num++;
-// }
-
-// do {
-//     console.log('num');
-//     num++;
-// }
-// while (num < 55);
-
-// let num = 50;
-
-// for (let i = 1; i < 18; i++) {
-//     if (i === 6) {
-//         //break;
-//         continue;
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//        border: 'black',
+//        bg: 'red' 
+//     },
+//     makeTest: function () {
+//         console.log('hello');
 //     }
-
-//     console.log (5);
-// }
-
-// let num = 20;
-
-// function showFirstMessage(text) {
-//     console.log(text);
-//     num = 10;
-// }
-
-// showFirstMessage('hello');
-
-// function calc (a, b) {
-//     return a+b;
-// }
-
-// console.log(calc(4, 3));
-// console.log(calc(5, 6));
-// console.log(calc(10, 6));
-
-// function ret() {
-//     let num = 50;
-//     return num;
-// }
-
-// const anotherNum = ret();
-// console.log(anotherNum);
-
-// const logger = function() {
-//     console.log(666);
 // };
 
-// logger();
+// options.makeTest();
 
-// const calc = (a, b) => {
-//     console.log('1');
-//     return a + b ;
-// };
+// const {border, bg} = options.colors;
+// console.log(border);
 
-// const str = 'test';
+// console.log(Object.keys(options).length);
 
+// let counter = 0;
 
- // console.log(str[2] = 'd');
+// console.log(options);
 
-// console.log(str.toUpperCase());
-// console.log(str.toLowerCase());
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Svoistvo ${i} imeet znachenie ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Svoistvo ${key} imeet znachenie ${options[key]}`);
+//         counter++;
+//     }
+// }
 
-// const fruit = 'Some fruit';
-
-// console.log(fruit.indexOf('fruit'));
-
-// const logg = 'hello world';
-
-// console.log(logg.slice(-1));
-
-// console.log(logg.substring());
-
-// const num = 12.2;
-
-// console.log(Math.round(12.6));
-
-// const test = '12.7px';
-// console.log(parseInt(test));
-// console.log(parseFloat(test));
-
-
+// console.log(counter);
